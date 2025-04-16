02f337b90158275a9d302bc3c1ee893fee29473e
AtulSemwal963 <atulsemwal963@example.com>
2025-04-16 11:00:05 +0530

    Fixed Path issue in PDFViewer

--- app/services/[feature]/page.jsx
+++ app/services/[feature]/page.jsx
@@ -8,7 +8,7 @@ import 'react-pdf/dist/Page/TextLayer.css';
 import './flipcard.css';
 
 import FileUpload from './components/FileUpload';
-import PDFViewer from './components/PDFViewer';
+import PDFViewer from '@/app/services/[feature]/components/PDFViewer';
 import LoadingOverlay from './components/LoadingOverlay';
 import LessonPlanModal from './components/LessonPlanModal';
 import ContentViewer from './components/ContentViewer';
